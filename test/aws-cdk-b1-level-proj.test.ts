b30984fd71d3bf59cc4995628bbbc9c4f04a667e
paritosh <paritoshjoshi11@gmail.com>
2024-12-02 08:42:15 +0530

    Initial commit

---
+++ test/aws-cdk-b1-level-proj.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkB1LevelProj from '../lib/aws-cdk-b1-level-proj-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-b1-level-proj-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkB1LevelProj.AwsCdkB1LevelProjStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
